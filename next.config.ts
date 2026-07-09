b8df75433e3cfa7e9753422a8d302ea463c64365
Tanmay Raj <tr4236@gmail.com>
2026-07-09 17:36:20 +0530

    Add fixes for images in PYQs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/pyqs/**/*': ['./public/pyq-dataset/**/*'],
+    '/pyqs': ['./public/pyq-dataset/**/*'],
+    '/subjects/**/*': ['./public/pyq-dataset/**/*'],
+    '/api/**/*': ['./public/pyq-dataset/**/*']
+  }
 };
 
 export default nextConfig;
